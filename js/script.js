c8ca90e27410e42be59b71f0642e9b31339c6a4b
Joshua Sonde (sondej) <joshua_sonde@icloud.com>
2023-05-19 19:11:20 +0100

    site is now coming together, some links now work.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  const h2Element = document.querySelector(".animate-scale-in");
+  if (isElementInViewport(h2Element)) {
+    console.log("RUNNINGGG");
+    h2Element.style.animationPlayState = "running";
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
