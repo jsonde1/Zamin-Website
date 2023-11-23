10b406be65aee21198880ae4c816fd02a2db95f7
Joshua Sonde (sondej) <joshua_sonde@icloud.com>
2023-11-23 19:53:57 +0000

    gallary is responsive. header sticky on some pages

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ allLinks.forEach(function (link) {
   });
 });
 
-/*function isElementInViewport(element) {
+/* function isElementInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
     rect.top >= 0 &&
@@ -21,8 +21,27 @@ allLinks.forEach(function (link) {
       (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
-}*/
+} */
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 function handleScroll() {
   const h2Element = document.querySelector(".animate-scale-in");
   if (isElementInViewport(h2Element)) {
